fcabff023be129c05b93bad73fb7a002539a6e27
Keith Perkins <keith.perkins@cnu.edu>
2024-09-09 16:38:36 -0400

    update gradle bits, removed extraneous layout

--- app/src/main/java/com/materialapp1/example/perkins/animation_demo/MainActivity.java
+++ app/src/main/java/com/materialapp1/example/perkins/animation_demo/MainActivity.java
@@ -35,7 +35,7 @@ public class MainActivity extends AppCompatActivity implements Animation.Animati
 
     public void doAnimate(View view) {
         animateit();
-        but.startAnimation(animRotate1);
+//        but.startAnimation(animRotate1);
     }
 
     public void doAnimate1(View view) {
